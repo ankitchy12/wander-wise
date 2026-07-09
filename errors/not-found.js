58022d3328246c9475bdb02c2ba722c37ce59c03
Ankit Chaudhary <Ankitchy158@gmail.com>
2026-07-09 16:57:06 +0545

    added and update user.js validators

--- errors/not-found.js
+++ errors/not-found.js
@@ -1,7 +1,7 @@
-export class NoteFoundError extends Error {
-    constructor(message = 'Resource not found') {
-        super(message);
-        this.name = 'NoteFoundError';
-        this.statusCode = 404;
-    }
+export class NotFoundError extends Error {
+  constructor(message = 'Resource not found') {
+    super(message);
+    this.name = 'NotFoundError';
+    this.statusCode = 404;
+  }
 }
